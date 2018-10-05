485e8d261e0a579e5912d541057d6d30485ca18c
jjurman <jjurman@constantcontact.com>
2018-10-04 20:08:45 -0400

    some cleanup

--- elements/digit.js
+++ elements/digit.js
@@ -4,15 +4,11 @@ const html = Tram.html({
 })
 
 module.exports = (attrs) => {
-  const style = `
-
-  `
-
   const binary = parseInt(attrs.value).toString(2)
   const paddedValue = '0000'.slice(0, 4 - binary.length).concat(binary)
 
   return html`
-    <div style=${style} >
+    <div>
       <led size="3em" ${paddedValue[0] === '1' ? 'on' : ''} />
       <led size="3em" ${paddedValue[1] === '1' ? 'on' : ''} />
       <led size="3em" ${paddedValue[2] === '1' ? 'on' : ''} />
--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const Tram = require('tram-one')
 require("babel-polyfill")
 
-const app = new Tram()
+const app = new Tram({
+  defaultRoute: '/'
+})
 app.addRoute('/', require('./pages/home'))
-app.addRoute('/404', require('./pages/404'))
 app.addActions({
   clock: require('./actions/clock'),
   ticker: require('./actions/ticker')
